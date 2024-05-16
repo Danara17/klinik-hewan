30efbbcee51c04fccfeaf6db6d58e22acc90acaf
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-05-16 13:17:29 +0800

    feat(Home) : Update Home component

--- resources/js/Components/HomeComponent/Hero.jsx
+++ resources/js/Components/HomeComponent/Hero.jsx
@@ -1,19 +1,88 @@
 import React from "react";
+import pet from "/public/static/bowl.png";
+import guk from "/public/static/guk.png";
 
 export default function Hero() {
+    const loginRoute = () => {
+        window.location.href = "/login";
+    };
     return (
         <>
-            <div className="hero min-h-screen bg-white flex justify-center items-center">
-                <div className="hero-content text-center ">
-                    <div className="max-w-md">
-                        <h1 className="text-5xl font-bold text-blue-700">
-                            Welcome to Pawspital
-                        </h1>
-                        <p className="py-6 text-blue-600">
-                            Provident cupiditate voluptatem et in. Quaerat
-                            fugiat ut assumenda excepturi exercitationem quasi.
-                            In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+            <div className="min-h-screen bg-white flex justify-center items-center">
+                <div className="flex flex-col max-w-fit gap-10  justify-center items-center py-28">
+                    <div className="flex flex-col gap-12 p-[42px]">
+                        <div className=" flex justify-center items-center text-center ">
+                            <div className="max-w-xl flex flex-col mb-12 gap-6 justify-center items-center">
+                                <div className="flex flex-col">
+                                    <h1 className="text-5xl font-bold text-blue-700 ">
+                                        Konsultasi Digital Hewan
+                                    </h1>
+                                    <h1 className="text-5xl font-bold text-blue-700">
+                                        Pawspital
+                                    </h1>
+                                </div>
+                                <button
+                                    onClick={loginRoute}
+                                    className="border py-4 px-12 max-w-full rounded-md bg-blue-600 text-white text-xl"
+                                >
+                                    Urus Sekarang
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center gap-12">
+                            <div className="hidden md:inline">
+                                <img src={guk} alt="" className="h-[32rem]" />
+                            </div>
+                            <div className="max-w-md">
+                                <h1 className="text-2xl font-bold text-blue-700 ">
+                                    Layanan Konsultasi Hewan
+                                </h1>
+                                <p className="py-6 text-blue-600">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Fugiat itaque quis cumque
+                                    illo, harum non animi! Tempore nostrum
+                                    repudiandae, repellendus tenetur ips
+                                </p>
+
+                                <p className="py-6 text-blue-600">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Fugiat itaque quis cumque
+                                    illo, harum non animi! Tempore nostrum
+                                    repudiandae, repellendus tenetur ips
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center gap-6">
+                            <div className="max-w-md flex flex-col gap-2">
+                                <h1 className="text-2xl font-bold text-blue-700 ">
+                                    Layanan Konsultasi Hewan
+                                </h1>
+                                <p className="py-6 text-blue-600">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Fugiat itaque quis cumque
+                                    illo, harum non animi! Tempore nostrum
+                                    repudiandae, repellendus tenetur ips
+                                </p>
+
+                                <p className="py-6 text-blue-600">
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Fugiat itaque quis cumque
+                                    illo, harum non animi! Tempore nostrum
+                                    repudiandae, repellendus tenetur ips
+                                </p>
+                            </div>
+                            <div className="flex  ">
+                                <div className="hidden md:inline">
+                                    <img
+                                        src={pet}
+                                        alt=""
+                                        className="h-[32rem]"
+                                    />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/js/Components/HomeComponent/Navbar.jsx
+++ resources/js/Components/HomeComponent/Navbar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Logo from "/public/static/logo.png";
 
 export default function Navbar() {
+    const loginRoute = () => {
+        window.location.href = "/login";
+    };
     return (
         <>
             <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a
-                        href="/login"
+                        href="/"
                         class="flex items-center space-x-3 rtl:space-x-reverse"
                     >
                         <img src={Logo} class="h-8" alt="Flowbite Logo" />
@@ -17,15 +20,18 @@ export default function Navbar() {
                     </a>
                     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                         <button
+                            onClick={loginRoute}
                             type="button"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
+                            font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                             Get started
                         </button>
                         <button
                             data-collapse-toggle="navbar-sticky"
                             type="button"
-                            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-10
+                             focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                             aria-controls="navbar-sticky"
                             aria-expanded="false"
                         >
@@ -51,36 +57,37 @@ export default function Navbar() {
                         class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                         id="navbar-sticky"
                     >
-                        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul
+                            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50
+                         md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 
+                         md:dark:bg-gray-900 dark:border-gray-700"
+                        >
                             <li>
                                 <a
-                                    href="#"
-                                    class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                    aria-current="page"
+                                    href="/article"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
+                                     md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
+                                      dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
-                                    Home
+                                    Article
                                 </a>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    href="/about"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
+                                     md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
+                                      dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     About
                                 </a>
                             </li>
                             <li>
                                 <a
-                                    href="#"
-                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Services
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    href="/faq"
+                                    class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent
+                                     md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700
+                                      dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Contact
                                 </a>
