8368224159c0f20dd2317d979c28c2f996ceea94
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-05-16 13:18:57 +0800

    feat(about us) : create about page layout

---
+++ resources/js/Pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/HomeComponent/Navbar";
+import Content from "../Components/AboutUsComponent/Content";
+
+export default function AboutUs() {
+    return (
+        <>
+            <Navbar />
+            <Content />
+        </>
+    );
+}
