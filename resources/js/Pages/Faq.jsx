11d4b1a97262c28ce655e29879402ea01e5d9ee8
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-05-16 13:18:06 +0800

    feat(FAQ) : create faq content layout

---
+++ resources/js/Pages/Faq.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/HomeComponent/Navbar";
+import FaqContent from "../Components/FaqComponent/FaqContent";
+import Footer from "../Components/HomeComponent/Footer";
+
+export default function Faq() {
+    return (
+        <>
+            <Navbar />
+            <FaqContent />
+            <Footer />
+        </>
+    );
+}
