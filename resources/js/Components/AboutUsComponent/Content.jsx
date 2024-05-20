f63e5592beb5ef80f24fa556085bef704712bfc0
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2024-05-20 17:10:25 +0800

    feat(About Us) : Create component for page About Us

--- resources/js/Components/AboutUsComponent/Content.jsx
+++ resources/js/Components/AboutUsComponent/Content.jsx
@@ -1,9 +1,219 @@
+import { Github, Instagram, Linkedin } from "lucide-react";
 import React from "react";
 
 export default function Content() {
     return (
         <>
-            <div className="py-28">Content About</div>
+            <div className="py-28">
+                <div className="max-w-screen-xl mx-auto p-5 py-4">
+                    <div className="flex flex-col gap-12">
+                        <div className="text-5xl font-semibold">
+                            Meet Our Team
+                        </div>
+                        <div className="flex flex-wrap gap-10 md:gap-20">
+                            <div className=" w-[300px] rounded-[36px] shadow-2xl h-96 flex flex-col gap-4 justify-center items-center p-8">
+                                <img
+                                    className="rounded-full w-28 h-28"
+                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                    alt=""
+                                />
+                                <div className="flex flex-col justify-center text-wrap text-center items-center gap-4">
+                                    <span className="text-2xl">
+                                        Danara Dhasa Caesa
+                                    </span>
+                                    <span className="text-wrap">
+                                        Universitas 17 Agustus 1945 Surabaya
+                                    </span>
+                                </div>
+
+                                <div className="flex gap-7 pt-8">
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Github />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Linkedin />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Instagram />
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className=" w-[300px] rounded-[36px] shadow-2xl h-96 flex flex-col gap-4 justify-center items-center p-8">
+                                <img
+                                    className="rounded-full w-28 h-28"
+                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                    alt=""
+                                />
+                                <div className="flex flex-col justify-center text-wrap text-center items-center gap-4">
+                                    <span className="text-2xl">
+                                        Muhamad Rafi Prabowo
+                                    </span>
+                                    <span className="text-wrap">
+                                        Universitas Muhammadiyah Malang
+                                    </span>
+                                </div>
+
+                                <div className="flex gap-7 pt-8">
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Github />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Linkedin />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Instagram />
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className=" w-[300px] rounded-[36px] shadow-2xl h-96 flex flex-col gap-4 justify-center items-center p-8">
+                                <img
+                                    className="rounded-full w-28 h-28"
+                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                    alt=""
+                                />
+                                <div className="flex flex-col justify-center text-wrap text-center items-center gap-4">
+                                    <span className="text-2xl">
+                                        Geraldi Nathan
+                                    </span>
+                                    <span className="text-wrap">
+                                        Universitas Muhammadiyah Malang
+                                    </span>
+                                </div>
+
+                                <div className="flex gap-7 pt-8">
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Github />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Linkedin />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Instagram />
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className=" w-[300px] rounded-[36px] shadow-2xl h-96 flex flex-col gap-4 justify-center items-center p-8">
+                                <img
+                                    className="rounded-full w-28 h-28"
+                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                    alt=""
+                                />
+                                <div className="flex flex-col justify-center text-wrap text-center items-center gap-4">
+                                    <span className="text-2xl">
+                                        M. Arya Suherman
+                                    </span>
+                                    <span className="text-wrap">
+                                        Universitas Micro Skill
+                                    </span>
+                                </div>
+
+                                <div className="flex gap-7 pt-8">
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Github />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Linkedin />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Instagram />
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className=" w-[300px] rounded-[36px] shadow-2xl h-96 flex flex-col gap-4 justify-center items-center p-8">
+                                <img
+                                    className="rounded-full w-28 h-28"
+                                    src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                                    alt=""
+                                />
+                                <div className="flex flex-col justify-center text-wrap text-center items-center gap-4">
+                                    <span className="text-2xl">
+                                        Jesia Esa Christanti
+                                    </span>
+                                    <span className="text-wrap">
+                                        Universitas Terbuka Yogyakarta
+                                    </span>
+                                </div>
+
+                                <div className="flex gap-7 pt-8">
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Github />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Linkedin />
+                                    </a>
+                                    <a
+                                        href=""
+                                        target="_blank"
+                                        className="hover:text-blue-500"
+                                    >
+                                        <Instagram />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
