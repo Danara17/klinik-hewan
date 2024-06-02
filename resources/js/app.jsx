60061ddaa05c9ad4b2f05a8c3579d3f61bc352c8
Danara17 <dcdanara@gmail.com>
2024-06-03 04:00:29 +0700

    Poseng vite JS ERROR ada yang bisa bantu kah wkwk. app.js

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"; // Note the correction from react
 import { createInertiaApp } from "@inertiajs/inertia-react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { AuthProvider } from "./Components/contexts/AuthContext"; // Correct importimport "flowbite"; // Import Flowbite if needed
+import "flowbite";
 import "./bootstrap"; // Import other necessary setup files
 import "../css/app.css"; // Import your CSS
 
